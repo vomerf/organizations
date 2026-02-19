c1148cef2d87cdc6ff39814de7aebdc6eab10858
Nikita <vomerff@gmail.com>
2026-02-19 12:51:52 +0700

    create base model for repo

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,12 +1,50 @@
 from app.config.database import Base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Generic, TypeVar, Type
+
+ModelType = TypeVar("ModelType", bound=Base)
 
 
-class BaseRepo:
-    model_class: Base
+class BaseRepo(Generic[ModelType]):
+    model_class: Type[ModelType]
 
-    async def get_data_by_id(self):
-        pass
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
 
+    async def get_data_by_id(self, obj_id: int):
+        stmt = select(self.model_class).where(self.model_class.id == obj_id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
 
-    async def get_data_by_name(self):
-        pass
+    async def get_all(self):
+        stmt = select(self.model_class)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_data_by_name(self, name: str):
+        stmt = select(self.model_class).where(self.model_class.name == name)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_data_by_params(self, **params: dict):
+        stmt = select(self.model_class)
+
+        for field, value in params.items():
+            if not hasattr(self.model_class, field):
+                raise ValueError(f"{field} is not a valid column")
+
+            column = getattr(self.model_class, field)
+            stmt = stmt.where(column == value)
+
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def create(self, **data: dict):
+        obj = self.model_class(**data)
+        self.session.add(obj)
+        await self.session.flush()
+        return obj
+    
+    async def delete(self, obj):
+        await self.session.delete(obj)
