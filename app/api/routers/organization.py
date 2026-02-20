390fcbd608ac12d14cfee33e2f4dbe7ee63c4742
Nikita <vomerff@gmail.com>
2026-02-20 13:47:37 +0700

    add layer service

--- app/api/routers/organization.py
+++ app/api/routers/organization.py
@@ -1,23 +1,20 @@
-import math
-
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
-
 
 from app.config.database import get_session
 from app.config.settings import settings
-from app.models import Building, Organization, OrganizationPhone
 from app.repositories.organization import OrganizationRepo
 from app.schemas.organization import OrganizationActivityOut, OrganizationOut
+from app.services.organization import OrganizationService
+
 
 router = APIRouter(tags=['Organization'])
 
 
 @router.get("/organizations/by_building/{building_id}", response_model=list[OrganizationOut])
 async def get_organizations_by_building(building_id: int, session: AsyncSession = Depends(get_session)):
-    repo = OrganizationRepo(session)
-    orgs = await repo.get_data_by_build_id(building_id)
+    org_service = OrganizationService()
+    orgs = await org_service.get_organizations_by_building(session, building_id)
 
     if not orgs:
         raise HTTPException(status_code=404, detail="В здании нету организаций")
@@ -36,8 +33,8 @@ async def organizations_by_activity(
     activity_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    repo = OrganizationRepo(session)
-    orgs = await repo.get_data_by_activity_id(activity_id)
+    org_service = OrganizationService()
+    orgs = await org_service.get_organizations_by_activity(session, activity_id)
 
     if not orgs:
         raise HTTPException(status_code=404, detail="Для данной деятельности не нашлось ни одной организации")
@@ -49,8 +46,8 @@ async def organizations_by_activity(
 
 @router.get("/organizations_by_nested_activity", response_model=list[OrganizationActivityOut])
 async def get_organizations_by_nested_activity(activity_id: int, session: AsyncSession = Depends(get_session)):
-    repo = OrganizationRepo(session)
-    orgs = await repo.organizations_by_nested_activity(activity_id)
+    org_service = OrganizationService()
+    orgs = await org_service.get_organizations_by_nested_activity(session, activity_id)    
     return [
         OrganizationActivityOut(
             organization=row["organization_name"],
@@ -66,12 +63,11 @@ async def get_organization_by_id(
     organization_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    repo = OrganizationRepo(session)
-    org = await repo.get_organization_by_id(organization_id)
+    org_service = OrganizationService()
+    org = await org_service.get_organization_by_id(session, organization_id)    
     
     if not org:
         raise HTTPException(status_code=404, detail="Организация не найдена")
-
     return OrganizationOut(name=org['name'], phones=org['phones'])
 
 
@@ -80,8 +76,8 @@ async def get_organization_by_name(
     organization_name: str = Query(..., description="Название организации"),
     session: AsyncSession = Depends(get_session)
 ):
-    repo = OrganizationRepo(session)
-    org = await repo.get_organization_by_name(organization_name)
+    org_service = OrganizationService()
+    org = await org_service.get_organization_by_name(session, organization_name)    
     
     if not org:
         raise HTTPException(status_code=404, detail="Организация не найдена")
@@ -96,17 +92,11 @@ async def get_organizations_nearby(
     radius_m: float = Query(settings.DEFAULT_RADIUS, gt=0, description="Радиус поиска в метрах"),
     session: AsyncSession = Depends(get_session)
 ):
+    org_service = OrganizationService()
+    orgs = await org_service.get_organizations_nearby(session, lat, lon, radius_m)
 
-    point = func.ST_SetSRID(func.ST_MakePoint(lon, lat), 4326)
-    point_geog = func.geography(point)
-
-    building_point_geog = func.geography(
-        func.ST_SetSRID(func.ST_MakePoint(Building.longitude, Building.latitude), 4326)
-    )
-    repo = OrganizationRepo(session)
-    orgs = await repo.get_organizations_nearby(point_geog, building_point_geog, radius_m)
     if not orgs:
-        raise HTTPException(status_code=404, detail="В радиусе {radius_m}м нет ни одной организации")
+        raise HTTPException(status_code=404, detail=f"В радиусе {radius_m}м нет ни одной организации")
     
     return [OrganizationOut(name=name, phones=phones) for name, phones in orgs]
 
