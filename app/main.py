49e7f9537b7b7aa5a45ee28b47a114f0d4787e98
Nikita Efremov <101176519+vomerf@users.noreply.github.com>
2026-04-08 21:58:43 +0700

    add prometheus (#11)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.routers import organization_router
-from app.deps import get_token_header
+# from app.deps import get_token_header
 
-app = FastAPI(dependencies=[Depends(get_token_header)])
+
+app = FastAPI()
+# app = FastAPI(dependencies=[Depends(get_token_header)])
 
 app.include_router(organization_router)
+
+Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=["/metrics", "/docs", "/openapi.json", "/redoc"],
+).instrument(app).expose(
+    app,
+    include_in_schema=False,
+    endpoint="/metrics",
+)
